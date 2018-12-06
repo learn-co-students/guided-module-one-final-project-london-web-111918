dc3791a4608763056919e3aea68d24c7cb111c59
atkinsonholly <atkinsonholly@gmail.com>
2018-12-06 15:18:29 +0000

    parse_data.rb methods updated

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,41 +3,18 @@ require 'net/http'
 require 'json'
 require 'rest-client'
 
-def get_data_from_api_rates(ticker)
+def get_data_from_api_rates(ticker) #gets tokens, currencies and rates
   response_string = RestClient.get("https://rest.coinapi.io/v1/exchangerate/" + ticker + "?apikey=9EE71748-0D09-4105-9B03-ED532F53A3F3")
   response_hash = JSON.parse(response_string)
 end
 
-
-def get_data_from_api_symbols #note: symbols data includes the volume information
+def get_data_from_api_symbols #gets volumes by token and exchange
   response_string = RestClient.get("https://rest.coinapi.io/v1/symbols?apikey=9EE71748-0D09-4105-9B03-ED532F53A3F3")
   response_hash = JSON.parse(response_string)
   #puts response_hash
 end
 
-# [{"symbol_id"=>"DERIBIT_OPT_BTC_USD_190125_6250_P", "exchange_id"=>"DERIBIT", "symbol_type"=>"OPTION", "asset_id_base"=>"BTC", "asset_id_quote"=>"USD", "option_type_is_call"=>false, "option_strike_price"=>6250.0, "option_contract_unit"=>1.0, "option_exercise_style"=>"EUROPEAN", "option_expiration_time"=>"2019-01-25T08:00:00.0000000Z", "data_start"=>"2018-12-03", "data_end"=>"2018-12-04", "data_quote_start"=>"2018-12-03T04:04:54.8358106Z", "data_quote_end"=>"2018-12-04T00:00:00.0000000Z", "data_orderbook_start"=>"2018-12-03T04:04:54.8358106Z", "data_orderbook_end"=>"2018-12-04T00:00:00.0000000Z", "volume_1hrs"=>0.0, "volume_1hrs_usd"=>0.0, "volume_1day"=>0.0, "volume_1day_usd"=>0.0, "volume_1mth"=>0.0, "volume_1mth_usd"=>0.0}...]
-
 def get_data_from_api_exchanges #exchange IDs, websites and date information
   response_string = RestClient.get("https://rest.coinapi.io/v1/exchanges?apikey=9EE71748-0D09-4105-9B03-ED532F53A3F3")
   response_hash = JSON.parse(response_string)
-  #puts response_hash
 end
-
-  # [{
-  #   "exchange_id": "OKCOIN_CNY",
-  #   "website": "https://www.okcoin.cn/",
-  #   "name": "OKCoin CNY",
-  #   "data_start": "2013-06-12",
-  #   "data_end": "2018-03-09",
-  #   "data_quote_start": "2015-02-15T12:53:50.3430000Z",
-  #   "data_quote_end": "2018-03-09T00:00:00.0000000Z",
-  #   "data_orderbook_start": "2015-02-15T12:53:50.3430000Z",
-  #   "data_orderbook_end": "2018-03-09T00:00:00.0000000Z",
-  #   "data_trade_start": "2013-06-12T14:24:24.0000000Z",
-  #   "data_trade_end": "2017-11-01T00:00:00.0000000Z",
-  #   "data_trade_count": 2085401174,
-  #   "data_symbols_count": 2
-  # }...]
-
-#latest
-#https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/latest?period_id=1MIN
--- lib/parse_data.rb
+++ lib/parse_data.rb
@@ -9,7 +9,6 @@ require 'pry'
 
 def save_tokens_to_db
   Token.destroy_all
-
   # Get Bitcoin data and create Bitcoin tokens
   response_hash = get_data_from_api_rates("BTC")
   tokens_BTC = response_hash["rates"].each { |element| Token.create({name: "Bitcoin", slug: "BTC", rate: element["rate"], currency: element["asset_id_quote"]}) }
@@ -25,7 +24,6 @@ def save_tokens_to_db
   # Get Stellar data and create Stellar tokens
   response_hash_5 = get_data_from_api_rates("XLM")
   tokens_XRP = response_hash_3["rates"].each { |element| Token.create({name: "Ripple", slug: "XLM", rate: element["rate"], currency: element["asset_id_quote"]}) }
-
 end
 
 ##### Save exchanges
@@ -45,7 +43,6 @@ def save_token_exchanges_to_db
   TokenExchange.destroy_all
   response_hash_token_exchanges = get_data_from_api_symbols
   response_hash_token_exchanges = response_hash_token_exchanges.select {|element| element["symbol_type"] == "SPOT"}
-
   response_hash_token_exchanges.each do |element|
     token = Token.find_by(slug: element["asset_id_base"], currency: element["asset_id_quote"])
     exchange = Exchange.find_by(name: element["exchange_id"])
@@ -55,39 +52,154 @@ def save_token_exchanges_to_db
   end
 end
 
-##### Methods to return information to user
 
+##### Methods to return information to user through command_line_interface
+
+
+##### METHOD ONE
 def current_rate_for_token
 
-    token_list = ["BTC", "ETH", "BCH", "XRP", "XLM"]
-    puts "Please choose from one of the following tokens: Bitcoin (BTC), Ethereum (ETH), Bitcoin Cash (BCH), Ripple (XRP), Stellar (XLM)"
-    user_input_token = "BTC"
-    if user_input_token.class != String || user_input_token.length > 3
-      raise ArgumentError.new('Input must be a 3 character string')
-      return true
-    elsif user_input_token == "Exit" || user_input_token == "exit" || user_input_token == "EXIT"
-      return true
-    elsif token_list.include?(user_input_token) == false
-      raise ArgumentError.new('Input must be one of: BTC, ETH, BCH, XRP, XLM')
-    else
-      array = ["ETH", "USDC", "XLM", "USDT", "GBP", "EUR", "USD", "BRL", "XRP","LTC", "TUSD", "DAI", "RUB", "JPY", "NZD", "GUSD", "DOGE", "PAX", "TRY", "EURS", "KRW", "CAD", "HUSD", "MXN", "PLN", "KST", "SLL", "AUD", "PHP", "ZAR", "NGN", "IDR", "NMC", "MYR", "UAH", "SGD", "NIS", "CNYX", "USNBT", "CZK", "THB", "DKKT", "NZDT", "CNNBT", "SEK", "RON", "NOK", "INR", "ILS", "HUF", "HRK", "HKD", "DKK", "CNY", "CHF", "BGN", "UST", "EURN", "VEN", "BTC", "WAVES", "ZEC", "BCH", "UNO", "HT", "EOS", "OKB", "BNB", "QASH", "RUR", "DASH", "NEO", "KCS", "ZBC", "CNET"]
+  token_list = ["BTC", "ETH", "BCH", "XRP", "XLM"]
+  puts "Please choose from one of the following tokens: Bitcoin (BTC), Ethereum (ETH), Bitcoin Cash (BCH), Ripple (XRP), Stellar (XLM)"
+  user_input_token = gets.chomp.upcase
+  while token_list.include?(user_input_token) == false
+    puts "Please select from: BTC, ETH, BCH, XRP, XLM"
+    user_input_token = gets.chomp.upcase
+  end
+  array = ["ETH", "USDC", "XLM", "USDT", "GBP", "EUR", "USD", "BRL", "XRP","LTC", "TUSD", "DAI", "RUB", "JPY", "NZD", "GUSD", "DOGE", "PAX", "TRY", "EURS", "KRW", "CAD", "HUSD", "MXN", "PLN", "KST", "SLL", "AUD", "PHP", "ZAR", "NGN", "IDR", "NMC", "MYR", "UAH", "SGD", "NIS", "CNYX", "USNBT", "CZK", "THB", "DKKT", "NZDT", "CNNBT", "SEK", "RON", "NOK", "INR", "ILS", "HUF", "HRK", "HKD", "DKK", "CNY", "CHF", "BGN", "UST", "EURN", "VEN", "BTC", "WAVES", "ZEC", "BCH", "UNO", "HT", "EOS", "OKB", "BNB", "QASH", "RUR", "DASH", "NEO", "KCS", "ZBC", "CNET"]
+  puts "Please enter a currency"
+  user_input_currency = gets.chomp.upcase
+  while array.include?(user_input_currency) == false
+    puts "Please enter a valid 3 character currency code"
+    user_input_currency = gets.chomp.upcase
+  end
+  token = Token.all.find_by(slug: user_input_token, currency: user_input_currency)
+  rate = token.rate
+  name = token.name
+  puts "The latest rate for #{name} in #{user_input_currency} is #{rate}."
+  return true
+end
 
-      puts "Please enter a currency"
-      user_input_currency = "USD"
-      if user_input_currency.class != String || user_input_currency.length > 3
-        raise ArgumentError.new('Input must be a 3 character string')
-        return true
-      elsif user_input_currency == "Exit" || user_input_currency== "exit" || user_input_currency == "EXIT"
-        return true
-      elsif array.include?(user_input_currency) == false
-        raise ArgumentError.new('Input must be an authorised currency')
-      else
-        token = Token.all.find_by(slug: user_input_token, currency: user_input_currency)
-        rate = token.rate
-        name = token.name
-        puts "The latest rate for #{name} in #{user_input_currency} is #{rate}."
-        return true
+
+##### METHOD TWO
+def show_all_exchanges_with_tokens_and_currencies
+
+  hash = {}
+  Exchange.all.each do | exchange |
+    my_hash = {}
+    hash[exchange.name] = my_hash
+    exchange.tokens.each do | token |
+      my_hash[token.name] = token.currency
+    end
+  end
+  puts hash
+  return true
+end
+
+
+##### METHOD THREE
+def show_exchanges_for_currency
+  array = ["ETH", "USDC", "XLM", "USDT", "GBP", "EUR", "USD", "BRL", "XRP","LTC", "TUSD", "DAI", "RUB", "JPY", "NZD", "GUSD", "DOGE", "PAX", "TRY", "EURS", "KRW", "CAD", "HUSD", "MXN", "PLN", "KST", "SLL", "AUD", "PHP", "ZAR", "NGN", "IDR", "NMC", "MYR", "UAH", "SGD", "NIS", "CNYX", "USNBT", "CZK", "THB", "DKKT", "NZDT", "CNNBT", "SEK", "RON", "NOK", "INR", "ILS", "HUF", "HRK", "HKD", "DKK", "CNY", "CHF", "BGN", "UST", "EURN", "VEN", "BTC", "WAVES", "ZEC", "BCH", "UNO", "HT", "EOS", "OKB", "BNB", "QASH", "RUR", "DASH", "NEO", "KCS", "ZBC", "CNET"]
+  puts "Please enter a currency"
+  user_input_currency = gets.chomp.upcase
+  while array.include?(user_input_currency) == false
+    puts "Please enter a valid 3 character currency code"
+    user_input_currency = gets.chomp.upcase
+  end
+  array = []
+  Exchange.all.each do | exchange |
+  exchange.tokens.each do | token |
+    if token.currency == user_input_currency
+      array << exchange.name
+    end
+  end
+  end
+  if array.length > 1
+  puts "The exchanges that trade in #{user_input_currency} are: #{array.sort.join(', ')}."
+  return true
+  elsif array.length == 0
+  puts "There are no exchanges that trade in #{user_input_currency}."
+  return true
+  elsif
+  puts "The only exchange that trades in #{user_input_currency} is #{array[0]}."
+  return true
+  end
+end
+
+
+##### METHOD FOUR
+def show_exchanges_for_token
+  token_list = ["BTC", "ETH", "BCH", "XRP", "XLM"]
+  puts "Please choose from one of the following tokens: Bitcoin (BTC), Ethereum (ETH), Bitcoin Cash (BCH), Ripple (XRP), Stellar (XLM)"
+  user_input_token = gets.chomp.upcase
+  while token_list.include?(user_input_token) == false
+    puts "Please select from: BTC, ETH, BCH, XRP, XLM"
+    user_input_token = gets.chomp.upcase
+  end
+  array = []
+  Token.all.each do |token|
+    if token.slug == user_input_token
+      token.exchanges.each do | exchange |
+        array << exchange.name
       end
     end
+  array = array.uniq.sort
+  end
+  if array.length > 1
+    puts "The exchanges that trade #{user_input_token} are: #{array.sort.join(', ')}."
+    return true
+  elsif array.length == 0
+    puts "There are no exchanges that trades #{user_input_token}."
+    return true
+  elsif
+    puts "The only exchange that has #{user_input_token} is #{array[0]}."
+    return true
+  end
 end
-current_rate_for_token
+
+
+##### METHOD FIVE
+def sort_exchanges_by_volume_for_token
+  token_list = ["BTC", "ETH", "BCH", "XRP", "XLM"]
+  puts "Please choose from one of the following tokens: Bitcoin (BTC), Ethereum (ETH), Bitcoin Cash (BCH), Ripple (XRP), Stellar (XLM)"
+  user_input_token = gets.chomp.upcase
+  while token_list.include?(user_input_token) == false
+    puts "Please select from: BTC, ETH, BCH, XRP, XLM"
+    user_input_token = gets.chomp.upcase
+  end
+  array_of_names = []
+  array_of_volumes = []
+  Token.all.each do | token |
+    if token.slug == user_input_token
+    token.token_exchanges.each do | token_exchange |
+        array_of_names << token_exchange.exchange.name
+        array_of_volumes << token_exchange.volume.round(1)
+      end
+    end
+  end
+  array = array_of_names.zip(array_of_volumes).sort_by{|name, vol| vol}
+  if array.length >= 5
+    puts "The top 5 exchanges in order of 1-day volume for #{user_input_token} are as follows: "
+    puts array[-1]
+    puts array[-2]
+    puts array[-3]
+    puts array[-4]
+    puts array[-5]
+    return true
+  elsif array.length < 5 && array.length >= 1 #very unlikely to be used for selected 5 tokens!
+    puts "The top exchanges in order of 1-day volume for #{user_input_token} are as follows: "
+    puts array
+    return true
+  elsif array.length == 0 #very unlikely to be used for selected 5 tokens!
+    puts "There are no exchanges."
+    return true
+  end
+end
+
+
+##### METHOD SIX
+def refresh_all_data
+  save_tokens_to_db
+  list_of_exchanges
+  save_token_exchanges_to_db
+end
